a27f1c3451b8c920a5604da85ae80757ebe02a7e
huntjack <hunt.jack01@gmail.com>
2023-05-30 15:02:21 -0700

    Method now works with oversized char arrays.
    The returned char array is right aligned, and if the array is too large, the beginning of the string will be marked with a null element.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String string = "Mr John Smith    ";
+        String string = "Mr John Smith      ";
         char[] charArray = string.toCharArray();
         charArray = urlify(charArray, 13);
         for(int i = 0; i < charArray.length; i++) {
@@ -8,6 +8,8 @@ public class Main {
         }
     }
     public static char[] urlify(char[] characters, int trueLength) {
+        int numberOfSpaces = countCharacters(characters, 0, trueLength, ' ');
+        int stopIndex = characters.length - 1 - (trueLength + numberOfSpaces * 2);
         int modified = characters.length - 1;
         char[] urlSpace = {'%', '2', '0'};
         for(int i = trueLength - 1; i >= 0; i--) {
@@ -21,6 +23,18 @@ public class Main {
                 }
             }
         }
+        if(stopIndex >= 0) {
+            characters[stopIndex] = Character.MIN_VALUE;
+        }
         return characters;
+    }
+    private static int countCharacters(char[] characters, int start, int end, char target) {
+        int count = 0;
+        for(int i = start; i < end; i++) {
+            if (characters[i] == target) {
+                count++;
+            }
+        }
+        return count;
     }
 }
